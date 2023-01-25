6ee0955ea1a6cab9916f345736dfcb34e38ebc2a
nakgyeom <himina1227@gmail.com>
2023-01-25 10:35:50 +0900

    init commit

---
+++ command/src/test/java/com/example/commond/CommondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
